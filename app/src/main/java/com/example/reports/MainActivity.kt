9817a60472544d6be128009b210ba56a178e8a3f
Gnezdilovnv <gnezdilov.n.v@gmail.com>
2026-07-03 23:53:32 +0300

    Initial commit: ReportsApp

---
+++ app/src/main/java/com/example/reports/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.reports
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        findViewById<Button>(R.id.btnTest).setOnClickListener {
+            Toast.makeText(this, "Приложение работает!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
